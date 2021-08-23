504359972384fa905fff2267fd71ae0ee6c45c73
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-23 19:37:48 +0530

    Ceated: SetUp for the employee Payroll app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'employee-payroll-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-payroll-angular');
+    expect(app.title).toEqual('employee-payroll');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('employee-payroll-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('employee-payroll app is running!');
   });
 });
